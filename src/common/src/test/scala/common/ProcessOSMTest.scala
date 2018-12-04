bf57561979fcf75a1be4b2b7b2c778749d266bf1
yiqing_jin <yiqing_jin@apple.com>
2018-12-04 13:18:44 -0800

    change to allow running locally

--- src/common/src/test/scala/common/ProcessOSMTest.scala
+++ src/common/src/test/scala/common/ProcessOSMTest.scala
@@ -1,20 +1,38 @@
 package osmesa.common
 
+import cats.implicits._
 import org.locationtech.geomesa.spark.jts._
-import org.scalatest._
+import org.scalatest.{FunSpec, Matchers}
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions.{lit, size => sqlSize}
 
 
 class ProcessOSMTest extends FunSpec with TestEnvironment with Matchers {
   import ss.implicits._
   ss.withJTS
 
-  val orcFile = getClass.getResource("/isle-of-man-latest.osm.orc").getPath
+//  val orcFile = getClass.getResource("/isle-of-man-latest.osm.orc").getPath
+  val orcFile = "/Users/yiqing_jin/data_dir/osm/orc/haiti-and-domrep-internal.osh.orc"
 
   val elements = ss.read.orc(orcFile)
   val nodes = ProcessOSM.preprocessNodes(elements).cache
-  val nodeGeoms = ProcessOSM.constructPointGeometries(nodes).cache
+  val nodeGeoms = ProcessOSM.constructPointGeometries(nodes).withColumn("minorVersion", lit(0)).cache
   val wayGeoms = ProcessOSM.reconstructWayGeometries(elements, nodes).cache
   val relationGeoms = ProcessOSM.reconstructRelationGeometries(elements, wayGeoms).cache
+
+  nodeGeoms.printSchema()
+  wayGeoms.printSchema()
+  relationGeoms.printSchema()
+
+  nodeGeoms
+    .union(wayGeoms.drop('geometryChanged).where(sqlSize('tags) > 0))
+    .union(relationGeoms)
+    //        .withColumn("wkt", ST_AsText('geom))
+    //        .drop('geom)
+    .repartition(1)
+    .write
+    .mode(SaveMode.Overwrite)
+    .orc("output/test2.orc")
 
   it("parses isle of man nodes") {
     info(s"Nodes: ${nodeGeoms.count}")
@@ -27,4 +45,6 @@ class ProcessOSMTest extends FunSpec with TestEnvironment with Matchers {
   it("parses isle of man relations") {
     info(s"Relations: ${relationGeoms.count}")
   }
+
+
 }
